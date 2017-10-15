1de300309982b774a656c32024a6c807e0339434
Irvin Alfaro <irvinalfaro11@gmail.com>
2017-10-15 10:50:39 +0700

    add new activity

--- src/pages/EventManagement.page.js
+++ src/pages/EventManagement.page.js
@@ -1,32 +1,18 @@
 import React,  { Component } from 'react';
-import Navigation from '../smart-components/Navigation';
-import { Link } from 'react-router';
-import RightSideNav from '../dumb-components/RightSideNav.component';
 import LeftSideNav from '../dumb-components/LeftSideNav.component';
-import FaListUI from 'react-icons/lib/fa/list-ul';
-import '../styles/eventManagement.page.scss';
+import '../styles/EventManagement.page.scss';
 class EventManagementPage extends Component {
   render () {
     return (
-      <div>
-        <div className="container-fluid">
-          <div className="row d-flex d-md-block flex-nowrap wrapper">
-            <div className="col-md-3 col-sm-1 float-right col-1 pl-0 pr-0 collapse width hide" id="sidebar">
-              <RightSideNav />
-            </div>
-            <div className="col-md-1 float-right col pt-3 mt-3">
-                <a id="mainToggler" data-target="#sidebar" data-toggle="collapse"><FaListUI size={32} id="icon" /></a>
-            </div>
-            <main className="row col-md-8 float-left col px-5 pt-2 main" id="main"> 
-              <div className="page-header mt-3 col-md-3">
-              <LeftSideNav />
-              </div>
-              <div className="float-left mt-3 col-md-9">
+        <div className="container-fluid newActivityContainer eventManagementContainer">
+          <div className="row d-flex d-md-block flex-nowrap wrapper eventManagementBody">
+            <main className="row col-12 main"> 
+              <div className="page-header mt-3 col-sm-2 col-lg-2">
+                <LeftSideNav />
               </div>
             </main>
+          </div>
         </div>
-        </div>
-      </div>
     );
   }
 }
--- src/smart-components/NewActivity.js
+++ src/smart-components/NewActivity.js
@@ -1,29 +1,19 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
-import { Link } from 'react-router';
-import RightSideNav from '../dumb-components/RightSideNav.component';
 import LeftSideNav from '../dumb-components/LeftSideNav.component';
-import FaListUI from 'react-icons/lib/fa/list-ul';
+import '../styles/NewActivity.scss';
 
-class NewEvent extends Component {
+class NewActivity extends Component {
     render () {
       return (
-        <div>
-          <div className="container-fluid">
-            <div className="row d-flex d-md-block flex-nowrap wrapper">
-                <div className="col-md-3 col-sm-1 float-right col-1 pl-0 pr-0 collapse width hide" id="sidebar">
-                  <RightSideNav />
-                </div>
-                <div className="col-md-1 float-right col pt-3 mt-3">
-                    <a id="mainToggler" data-target="#sidebar" data-toggle="collapse"><FaListUI size={32} id="icon" /></a>
-                </div>
-                <main className="row col-md-8 float-left col px-5 pt-2 main" id="main"> 
-                  <div className="page-header mt-3 col-md-3">
+        <div className="container-fluid newActivityContainer">
+          <div className="row d-flex d-md-block flex-nowrap wrapper newActivityBody">
+              <main className="row col-12 main"> 
+                <div className="page-header mt-3 col-sm-2 col-lg-2">
                   <LeftSideNav />
-                  </div>
-                  <div className="float-left mt-3 col-md-9">
+                </div>
+                <div className="mt-3 col-sm-8 col-lg-8">
                   <form>
-                    <legend className="mt-2">Event Information</legend>
+                    <legend className="mt-2">Activity Information</legend>
                     <hr />
                     <div className="input-group">
                       <span className="input-group-addon" id="basic-addon1">Title</span>
@@ -36,7 +26,7 @@ class NewEvent extends Component {
                     </div>
                     <br />
                     <div className="input-group">
-                    <span className="input-group-addon" id="basic-addon1">Schedule</span>
+                    <span className="" id="basic-addon1">Schedule</span>
                     </div>
                     <hr />
                     <br />
@@ -54,11 +44,10 @@ class NewEvent extends Component {
                     <input className="btn btn-primary ml-5" type="reset" value="Cancel" />
                   </form>
                   </div>
-                </main>
-            </div>
+              </main>
           </div>
-      </div>
+        </div>
       )
     }
 }
-export default NewEvent;
+export default NewActivity;
